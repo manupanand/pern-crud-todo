035a08d3c07094c4e1554202e52012842715ef0e
manupanand <manupanandindianinvestor@gmail.com>
2024-07-16 23:53:00 +0530

    sql database added

--- README.md
+++ README.md
@@ -22,11 +22,17 @@ npm run dev
 ```
 npm run start
 ```
-
+# Environment setup
+-add .env file in api-server and frontend
+-for Database
+            PORT=portnumber
+            DB=`url`
 # Things to do in this application
+    -add logging  service on seperate server
     -add loadbalancer 
     -add cache DB redis
     -modify crud on prisma
     -passport
     -crypto encrpt
-    
+    -add dns and architecture diagram
+    -eventually make this into a todoist like calender add etc project mangement 
--- api-server/index.js
+++ api-server/index.js
@@ -1,20 +1,24 @@
 const express= require("express");
 const cors=require("cors")
 const app= express();
-const PORT=3000
+require('dotenv').config()
+const PORT=process.env.PORT
+const DBurl=process.env.DB
 
+//middleware
 app.use(cors())
+app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.send({"message":"test connected"})
 })
-app.get('/login',async(req,res)={
-
+app.get('/login', async(req,res)=>{
+    res.send({"message":"login request"})
 })
-app.post('/signup',async(req,res)={
-
+app.post('/signup',async(req,res)=>{
+    res.send({message:"post request"})
 })
-app.post('/update',async(req,res)={
+app.post('/update',async(req,res)=>{
 
 })
 
